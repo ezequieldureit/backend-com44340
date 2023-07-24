0a997b433aec3492b35be7800286c51203f8548d
Ezequiel Duré <106278375+ezequieldureit@users.noreply.github.com>
2023-07-23 23:45:13 -0300

    Update main.js

--- 8. Moongose & Primera practica integradora/src/public/js/main.js
+++ 8. Moongose & Primera practica integradora/src/public/js/main.js
@@ -1,54 +1,6 @@
-// // Handle the "messageServer" event
-// const socket = io();
-
-// socket.on("mensajeServer", (productos) => {
-//   console.log("Productos recibidos:", productos);
-
-//   const productTableBody = document.getElementById("productTableBody");
-//   productTableBody.innerHTML = "";
-
-//   productos.forEach((producto) => {
-//     const row = document.createElement("tr");
-//     row.innerHTML = `
-//         <td>${producto.title}</td>
-//         <td>${producto.description}</td>
-//         <td>${producto.code}</td>
-//         <td>${producto.price}</td>
-//         <td>${producto.stock}</td>
-//         <td>${producto.category}</td>
-//         <td><img src="${producto.thumbnail}" alt="Imagen del producto" class=thumbnail-image></td>
-//       `;
-//     productTableBody.appendChild(row);
-//   });
-// });
-
-// // Handle product add form submission
-// const submitProductForm = document.getElementById("submitProduct");
-// submitProductForm.addEventListener("click", (event) => {
-//   event.preventDefault();
-
-//   const title = document.getElementById("title").value;
-//   const description = document.getElementById("description").value;
-//   const code = document.getElementById("code").value;
-//   const price = document.getElementById("price").value;
-//   const stock = document.getElementById("stock").value;
-//   const category = document.getElementById("category").value;
-//   const thumbnail = document.getElementById("thumbnail").value;
-
-//   socket.emit("product", {
-//     title,
-//     description,
-//     code,
-//     price,
-//     stock,
-//     category,
-//     thumbnail,
-//   });
-// });
-
 const socket = io();
 
-// Función para mostrar la lista de productos en la tabla
+// Function to display the list of products in the table
 function displayProducts(products) {
   const productTableBody = document.getElementById('productTableBody');
   productTableBody.innerHTML = '';
@@ -68,47 +20,47 @@ function displayProducts(products) {
   });
 }
 
-// Manejar el evento "mensajeServer" enviado por el servidor con la lista de productos inicial
+// Handle the "messageServer" event sent by the server with the initial product list
 socket.on('mensajeServer', (productos) => {
   console.log('Productos recibidos:', productos);
   displayProducts(productos);
 });
 
-// Manejar el evento "productoAgregado" enviado por el servidor cuando se agrega un producto
+// Handle the "productAdded" event sent by the server when a product is added
 socket.on('productoAgregado', (product) => {
   console.log('Producto agregado:', product);
-  // Obtener la lista de productos actualizada del servidor y mostrarla
+  // Get updated product list from server and display it
   socket.emit('getProducts');
 });
 
-// Agregar el manejo del evento "getProducts" enviado por el servidor en respuesta a "productoAgregado"
+// Add handling of the "getProducts" event sent by the server in response to "productAdded"
 socket.on('getProducts', (products) => {
   console.log('Lista de productos actualizada:', products);
   displayProducts(products);
 });
 
-// Manejar el evento "productDeleted" enviado por el servidor cuando se elimina un producto
+// Handle the "productDeleted" event sent by the server when a product is deleted
 socket.on('productDeleted', (products) => {
   console.log('Producto eliminado:', products);
   displayProducts(products);
 });
 
-// Manejar el evento "productError" enviado por el servidor en caso de errores
+// Handle the "productError" event sent by the server in case of errors
 socket.on('productError', (error) => {
   console.log('Error en el servidor:', error);
 });
 
-// Manejar el evento "getProducts" enviado por el servidor en respuesta a "productoAgregado"
+// Handle the "getProducts" event sent by the server in response to "productAdded"
 socket.on('getProducts', (products) => {
   console.log('Lista actualizada de productos:', products);
   displayProducts(products);
 });
-// Manejar el formulario de agregar producto
+// Manage the add product form
 const submitProductForm = document.getElementById('submitProduct');
 submitProductForm.addEventListener('click', (event) => {
   event.preventDefault();
 
-  // Obtener los datos del formulario y enviarlos al servidor
+  // Get the form data and send it to the server
   const title = document.getElementById('title').value;
   const description = document.getElementById('description').value;
   const code = document.getElementById('code').value;
@@ -128,7 +80,6 @@ submitProductForm.addEventListener('click', (event) => {
   });
 });
 
-// Resto del código ...
 
 function limpiarFormulario() {
   document.getElementById('miFormulario').reset();
